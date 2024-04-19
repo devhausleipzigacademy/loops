ac35d209873d38dc4c92bd431d01c3ada19ed324
luzieahrens <luzie.ahrens@gmail.com>
2024-04-19 12:04:18 +0200

    add supermarket

---
+++ 04_supermarket.js
@@ -0,0 +1,51 @@
+// 1. Task: Supermarket
+
+// You run a supermarket and need to track the items you have in store.
+
+// Create a new object called 'stock' that contains information about all items in store
+// The object should have the product items as the NAMES...
+// ...and the number of items in stock as the VALUES
+
+// You have the following items in stock:
+// bananas: 24
+// apples: 36
+// bread: 18
+// milk: 54
+
+// YOUR SOLUTION //
+// ... //
+
+// 2. Task: Selling items
+// Given is an array called 'shopping_cart' representing the shopping cart of a customer
+// the array contains more arrays in the form [ITEM, QUANTITY]
+shopping_cart = [["bananas", 2], ["milk", 3]]
+
+// Iterate over the array and substract the items from your stock
+
+// YOUR SOLUTION //
+// ... //
+
+// 3. Task: Money must be funny!
+
+// You need to keep track of the prices.
+// Create a new object called 'prices' that contains information about your prices
+// The object should have the product items as the NAMES...
+// ...and the price of the items as the VALUES
+
+// The prices of your items are the following:
+// bananas: 0.3
+// apples: 0.56
+// bread: 1.29
+// milk: 1.59
+
+// YOUR SOLUTION //
+// ... //
+
+// 4. Task: Cash Flow
+// Given is another shopping cart of a customer
+// Calculate the price for the shopping cart and save it in variable called 'price'
+shopping_cart2 = [["bananas", 4], ["milk", 1], ["bread", 2]]
+
+// YOUR SOLUTION //
+// ... //
+
